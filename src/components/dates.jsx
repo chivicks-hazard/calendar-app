9393102c8d7f2be32515af7261e6402e36a8c850
Victor Chgbo <chigbovcm2020@gmail.com>
2025-01-03 00:04:35 +0100

    Setting up the dates view

---
+++ src/components/dates.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+
+const DateHolder = () => {
+    return (
+        <div className="grid grid-rows-5 grid-cols-7 mx-auto w-full text-xl bg-gray-300 text-center">
+            <div className="p-5 border-x border-b-2 border-x-gray-500 border-b-black font-semibold">Monday</div>
+            <div className="p-5 border-x border-b-2 border-x-gray-500 border-b-black font-semibold">Tuesday</div>
+            <div className="p-5 border-x border-b-2 border-x-gray-500 border-b-black font-semibold">Wednesday</div>
+            <div className="p-5 border-x border-b-2 border-x-gray-500 border-b-black font-semibold">Thursday</div>
+            <div className="p-5 border-x border-b-2 border-x-gray-500 border-b-black font-semibold">Friday</div>
+            <div className="p-5 border-x border-b-2 border-x-gray-500 border-b-black font-semibold">Saturday</div>
+            <div className="p-5 border-x border-b-2 border-x-gray-500 border-b-black font-semibold">Sunday</div>
+            <div className='p-x-5 pb-5 border border-gray-500 flex flex-col content-start'><span>30</span></div>
+            <div className='p-x-5 pb-5 border border-gray-500 flex flex-col content-start'><span>31</span></div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>1</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>2</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>3</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>4</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>5</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>6</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>7</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>8</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>9</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>10</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>11</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>12</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>13</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>14</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>15</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>16</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>17</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>18</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>19</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>20</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>21</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>22</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>23</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>24</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>25</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>26</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>27</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>28</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>29</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>30</div>
+            </div>
+            <div className="p-x-5 pb-5 border border-gray-500 flex flex-col content-start">
+                <div>31</div>
+            </div>
+            <div className='p-x-5 pb-5 border border-gray-500 flex flex-col content-start'><span>1</span></div>
+            <div className='p-x-5 pb-5 border border-gray-500 flex flex-col content-start'><span></span></div>
+        </div>
+    )
+}
+
+export default DateHolder
